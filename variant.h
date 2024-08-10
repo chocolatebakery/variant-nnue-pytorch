1c82794596439dff5a32bdd574fa252315d676cc
chocolatebakery <chocobakery@protonmail.com>
2024-08-10 19:36:35 +0000

    shatranj values

--- variant.h
+++ variant.h
@@ -1,7 +1,7 @@
 #define FILES 8
 #define RANKS 8
-#define PIECE_TYPES 7
+#define PIECE_TYPES 6
 #define PIECE_COUNT 32
 #define POCKETS false
-#define KING_SQUARES 1
+#define KING_SQUARES 64
 #define DATA_SIZE 512
